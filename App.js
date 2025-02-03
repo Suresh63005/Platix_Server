43a6526fe32340865a40e22f601fac7cfed28051
diddiakhil <akhil.diddi@innoitlabs.com>
2025-02-03 18:14:25 +0530

    created login page

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 const express = require("express");
+const app = express();
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 const cors = require("cors");
-const bodyParser = require("body-parser");
-const { connectDB } = require("./config/db");
+const { connectDB,sequelize } = require("./config/db");
 const adminRoutes = require("./AdminRoutes/AdminRoute");
 const Admin = require("./Models/Adminmodel")
 
 dotenv.config();
-connectDB()
-;
-const app = express();
+connectDB();
 
 // Middleware
+app.use(morgan("dev"));
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());  // Use express.json() for parsing JSON requests
+app.use(express.urlencoded({ extended: true }));  // Handle URL-encoded data
 
+// Routes
+app.use("/admin", adminRoutes);
+
+// Test route
 app.get("/", (req, res) => {
   res.send("Server is running...");
 });
@@ -23,4 +27,12 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Database & tables created!");
+  })
+  .catch((err) => {
+    console.error("Unable to create the database:", err);
 });
