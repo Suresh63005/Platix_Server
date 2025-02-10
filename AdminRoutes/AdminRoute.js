c072f4ef473909c153eb4034decfb5667a4aebb5
diddiakhil <akhil.diddi@innoitlabs.com>
2025-02-10 18:50:41 +0530

    did changes

--- AdminRoutes/AdminRoute.js
+++ AdminRoutes/AdminRoute.js
@@ -5,7 +5,8 @@ const { createRole, viewRoles } = require("../AdminControllers/Roles.controller"
 const {upsertService,getAllServices,deleteService,serviceGetByid} = require("../AdminControllers/Services.controller");
 const upload = require("../utils/multer");
 const { getSettingsById, createOrUpdateSettings, FetchSettings, FetchSettingsById } = require('../AdminControllers/Settings.controller');
-const OrderReport=require("../AdminControllers/Reports/OrederReport.Controller")
+const OrderReport=require("../AdminControllers/Reports/OrederReport.Controller");
+const OrganizationTypeController = require("../AdminControllers/OrganizationType.controller")
 
 const router = express.Router();
 
@@ -24,6 +25,8 @@ router.get("/getbyid/:id", serviceGetByid);
 
 // Get All Services
 router.get("/allservices", getAllServices);
+router.post("/assign-service",OrganizationTypeController.assignServiceToOrganization)
+router.get("/getorgservices",OrganizationTypeController.getOrganizationService)
 
 // Delete a Service (Soft or Permanent)
 router.delete("/deleteservice/:id", deleteService);
--- AdminRoutes/ReportUser/User.router.js
+++
@@ -1,9 +0,0 @@
-const express=require("express")
-const UserController=require("../../AdminControllers/Reports/UserReports.Controller")
-const router=express.Router()
-
-router.post("/upsert",UserController.CreateUser,)
-router.get("/all",UserController.getAllUsers,)
-
-
-module.exports=router
