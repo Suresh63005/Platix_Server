cef0233b18939b51a02d927d5d4476a700af6908
SureshPyla2000 <suresh.pyla@innoitlabs.com>
2025-03-25 10:27:56 +0530

    cahngeas in asjabkjdakxsa

--- userRoutes/Dentist/Dentist.router.js
+++ userRoutes/Dentist/Dentist.router.js
@@ -8,9 +8,9 @@ router.get("/dentist/order/getbyid/:id",authMiddleware.verifyUser,DentistControl
 router.get("/dentist/order/report/:fromdate?/:todate?",authMiddleware.verifyUser,DentistController.orderReport) //1
 router.get("/dentist/order/payment-report/:fromdate?/:todate?",authMiddleware.verifyUser,DentistController.PaymentReports)//1
 router.get("/dentist/order/payment-report-getbyid/:id",authMiddleware.verifyUser,DentistController.ViewPaymentReportDetails)//1
-router.get('/dentist/order/search/:search',authMiddleware.verifyUser, DentistController.orderAndPaymentSearch);//1
+router.get("/dentist/order/search/:search",authMiddleware.verifyUser, DentistController.orderAndPaymentSearch);//1
 
 // for labrotory - radiology - material supply dentist screen
-router.get("/organization-details/getbyid/:id",authMiddleware.verifyUser,DentistController.getorganizationDetailsById)
+router.get("/dentist/organization-details/getbyid/:id",authMiddleware.isAuthenticated,DentistController.getorganizationDetailsById)
 
 module.exports=router
