24286fa15a9742164e6d4031f10ceeb166bb9426
rajesh <rajesh.swain@innoitlabs.com>
2025-03-26 12:26:05 +0530

    fixed dashboard  bug3

--- userRoutes/Notification.router.js
+++ userRoutes/Notification.router.js
@@ -4,7 +4,7 @@ const NotificationController=require("../userControllers/Notification.Controller
 const authMiddleWare=require("../Middlewares/auth")
 
 router.post("/notifications/upsert",NotificationController.createNotification)
-router.get("/notifications/get-notifications",authMiddleWare.verifyUser,NotificationController.getNotification)
-router.delete("/notifications/clear-all-notifications",authMiddleWare.verifyUser,NotificationController.clearAllNotifications)
+router.get("/notifications/get-notifications",authMiddleWare.isAuthenticated,NotificationController.getNotification)
+router.delete("/notifications/clear-all-notifications",authMiddleWare.isAuthenticated,NotificationController.clearAllNotifications)
 
 module.exports=router
