32ac0231a13a0c46b0bda998d1929ae268e16fa3
NARENDARKOREM <narendar.korem@innoitlabs.com>
2025-03-28 18:02:35 +0530

    sfafjdlkjdfsjldskfdjljl

--- userRoutes/Technician/technincian.router.js
+++ userRoutes/Technician/technincian.router.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const technicianDashboard = require('../../userControllers/Technician/TechnicianDashboard.Controller');
 const authMiddleware = require('../../Middlewares/auth');
+const upload = require("../../utils/multer")
 
 router.get("/technician/dashboard",authMiddleware.isAuthenticated,technicianDashboard.technicianDashboardData);
 router.get("/technician/order-status",authMiddleware.isAuthenticated,technicianDashboard.FetchTechnicianOrdersByStatus);
-router.get("/technician/order-details/:orderId",authMiddleware.isAuthenticated,technicianDashboard.ViewOrderDetails)
+router.get("/technician/order-details/:orderId",authMiddleware.isAuthenticated,technicianDashboard.ViewOrderDetails);
+router.put("/technician",authMiddleware.isAuthenticated,technicianDashboard.CancelAndCloseOrder);
+router.post("/technician/upload-images",authMiddleware.isAuthenticated,upload.array("images",5),technicianDashboard.UploadImagesByTechnician)
+router.get("/technician/search",authMiddleware.isAuthenticated,technicianDashboard.SearchAPI)
 
 module.exports = router;
