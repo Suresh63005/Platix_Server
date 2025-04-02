fdf70796a4f5d66b2ac365b96d41022424177c55
rajesh <rajesh.swain@innoitlabs.com>
2025-04-02 15:01:52 +0530

    changes

--- userRoutes/Dentist/Dentist.router.js
+++ userRoutes/Dentist/Dentist.router.js
@@ -2,6 +2,7 @@ const express=require("express")
 const router=express.Router();
 const DentistController=require("../../userControllers/Dentist/Dentist.Controller")
 const authMiddleware=require("../../Middlewares/auth")
+const dashBoardController = require("../../userControllers/DashBoard.Controller")
 
 
 router.route("/dentist/upsert/:cancel?").post(authMiddleware.isAuthenticated,DentistController.fromDentist).put(authMiddleware.isAuthenticated,DentistController.fromDentist);//1
@@ -13,5 +14,13 @@ router.get("/dentist/order/search/:search",authMiddleware.isAuthenticated, Denti
 router.get("/dentist/organization-details/getbyid/:id",authMiddleware.isAuthenticated,DentistController.getorganizationDetailsById);
 // cancelled order destroy
 router.delete("/dentist/delete/:status",authMiddleware.isAuthenticated,DentistController.cancelledAndDestroyOrder)
+
+router.get("/dashboard/all-orders",authMiddleware.isAuthenticated,dashBoardController.allOrders)
+router.get("/dashboard/all",authMiddleware.isAuthenticated,dashBoardController.all)
+router.get("/dashboard/all/organizations/search",authMiddleware.isAuthenticated, dashBoardController.searchOrganizations);
+router.get("/dashboard/all/organizationstype/search",authMiddleware.isAuthenticated, dashBoardController.searchByOrganizationType);
+router.get("/dashboard/status/:status/:userUUID",authMiddleware.isAuthenticated,dashBoardController.statusOrder)
+// for settings
+router.get("/settings",dashBoardController.termAndConditions)
 
 module.exports=router
