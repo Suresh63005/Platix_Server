a931805f3c375d8aabae2fe2146f6f1e59db715c
rajesh <rajesh.swain@innoitlabs.com>
2025-04-04 18:06:42 +0530

    changes

--- userControllers/Notification.Controller.js
+++ userControllers/Notification.Controller.js
@@ -1,4 +1,5 @@
 const Notification = require("../Models/Notification.model");
+const moment = require('moment');
 
 const createNotification = async (req, res) => {
     const { uid, datetime, title, description } = req.body;
@@ -18,18 +19,28 @@ const createNotification = async (req, res) => {
     }
   };
 
-  const getNotification=async(req,res)=>{
+  const getNotification = async (req, res) => {
     const { id } = req.user;
     try {
-      const notifications=await Notification.findAll({
-        where:{uid:id,deletedAt: null},
-      })
-      return res.status(200).json({success:true,notifications})
+      const notifications = await Notification.findAll({
+        where: { uid: id, deletedAt: null },
+        raw: true,  
+      });
+  
+      const formattedNotifications = notifications.map((notification) => {
+        notification.createdAt = moment(notification.createdAt).local().format('YYYY-MM-DD HH:mm:ss');
+        notification.updatedAt = moment(notification.updatedAt).local().format('YYYY-MM-DD HH:mm:ss');
+        notification.datetime = moment(notification.datetime).local().format('YYYY-MM-DD HH:mm:ss');
+        notification.deletedAt = moment(notification.deletedAt).local().format('YYYY-MM-DD HH:mm:ss');
+        return notification;
+      });
+  
+      return res.status(200).json({ success: true, notifications: formattedNotifications });
     } catch (error) {
       console.error("Error fetching notifications:", error);
-      return res.status(500).json({ success: false,  message: "Internal Server Error",  error: error.message });
+      return res.status(500).json({ success: false, message: "Internal Server Error", error: error.message });
     }
-  }
+  };
 
   // for clear notifications 
   const clearAllNotifications=async(req,res)=>{
